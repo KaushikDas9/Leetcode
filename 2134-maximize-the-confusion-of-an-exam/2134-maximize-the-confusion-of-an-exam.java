f50d82df07a7d811844ee608e62c2d203b609e09
KaushikDas9 <123364033+KaushikDas9@users.noreply.github.com>
2023-08-01 04:16:18 +0530

    Time: 13 ms (92.69%), Space: 43.5 MB (95.64%) - LeetHub

---
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        
+        int countT=0,countF=0,j=0,ans=0; 
+        for(int i=0; i<answerKey.length() ; i++)
+        {
+            if(answerKey.charAt(i)=='T') countT++;
+            else countF++;
+
+            if(k<Math.min(countT,countF))
+            {
+                if(answerKey.charAt(j++)=='T') countT--;
+                else countF--;               
+            }
+            ans=Math.max(ans,countT+countF);
+        }
+
+        return ans;
+    }
+}
