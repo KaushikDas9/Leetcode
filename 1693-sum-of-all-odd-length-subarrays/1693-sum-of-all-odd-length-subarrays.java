efb20b0e963e094c38dfccd33419c71d51f808aa
KaushikDas9 <123364033+KaushikDas9@users.noreply.github.com>
2023-08-07 06:30:13 +0530

    Update 1693-sum-of-all-odd-length-subarrays.java

--- 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.java
+++ 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.java
@@ -1,3 +1,24 @@
+//Brute Force
+
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum=0,ans=0;
+        for(int i=0 ; i<arr.length;i++)
+        {
+            int k=0;
+            for(int j=i;j<arr.length;j++)
+            {
+                sum += arr[j];
+                if(k++%2==0) ans+=sum;
+            }
+            sum=0;
+        }
+        return ans;
+    }
+}
+
+
+//Optimal
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
         int sum=0,n=arr.length;
