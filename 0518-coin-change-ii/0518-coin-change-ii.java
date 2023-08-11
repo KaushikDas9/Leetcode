73e7fca6e57d85ca4354b2f6feb1ee50e2d726ba
KaushikDas9 <123364033+KaushikDas9@users.noreply.github.com>
2023-08-11 12:59:30 +0530

    Time: 7 ms (58.33%), Space: 46.1 MB (21.42%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -3,8 +3,12 @@ class Solution {
     public static int find( int n , int[] arr , int tar,int dp[][])
     {
         if(tar == 0 ) return 1;
-        if(n < 0 ) return 0;
-       if( dp[n][tar] != -1 ) return dp[n][tar];
+        if( n==0 ) {
+            if( tar%arr[0] == 0 ) return 1;
+            else return 0;
+        }
+
+        if( dp[n][tar] != -1 ) return dp[n][tar];
 
            int take =0 ; 
            if( arr[n]<=tar ) take=  find(n , arr , tar - arr[n],dp );
@@ -20,8 +24,25 @@ class Solution {
 
         int n = coins.length;
         int dp[][] = new int[n][amount+1];
-        for( int i=0 ; i<n ;i++ ) { Arrays.fill(dp[i] , -1 );}
-        return find ( n-1,coins , amount ,dp);
+        // for( int i=0 ; i<n ;i++ ) { Arrays.fill(dp[i] , -1 );}
+        // return find ( n-1,coins , amount ,dp);
+
+        // memorization
+
+        for( int i=0 ; i<n ;i++ ) { dp[i][0]=1; }
+        for(int j=0;j<amount+1;j++) { if(j % coins[0] == 0) dp[0][j] =1; else dp[0][j]=0; }
+ 
+
+        for( int i=1 ; i<n ;i++ ) {
+            for( int tar=1 ; tar<amount+1 ;tar++ ) { 
+                int take =0 ; 
+                 if( coins[i]<=tar ) take=  dp[i][tar - coins[i]];
+                int ntake=  dp[i-1][tar];
+
+                dp[i][tar] = take + ntake ; 
+            }
+        }
+        return dp[n-1][amount] ;
 
     }
 }
