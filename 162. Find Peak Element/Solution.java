96749b6f18f6dac3c8ae645b3211dd8b4314d2f1
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2024-02-17 15:21:42 +0530

    Java Solution
            Submission Statistics:
            Question #: 162
            Language: Java
            Runtime: 0 ms
            Runtime percentile: 100.00
            Memory: 42.38 MB
            Memory percentile: 45.04

--- 162. Find Peak Element/Solution.java
+++ 162. Find Peak Element/Solution.java
@@ -2,10 +2,10 @@ class Solution {
     public int findPeakElement(int[] nums) {
 
         if( nums.length == 1) return 0;
-        if( nums.length == 2 ) {
-            if( nums[0] > nums[1] ) return 0;
-            return 1;
-        }
+        // if( nums.length == 2 ) {
+        //     if( nums[0] > nums[1] ) return 0;
+        //     return 1;
+        // }
 
         for( int i=0;i<nums.length-1;i++ ) { 
 
