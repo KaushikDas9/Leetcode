5859c55bc8089515d8ded0f62d399ba99d0842fe
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2024-02-19 02:25:58 +0530

    Java Solution
            Submission Statistics:
            Question #: 2327
            Language: Java
            Runtime: 4 ms
            Runtime percentile: 22.83
            Memory: 40.436 MB
            Memory percentile: 67.63

--- 2327. Largest Number After Digit Swaps by Parity/Solution.java
+++ 2327. Largest Number After Digit Swaps by Parity/Solution.java
@@ -1,8 +1,8 @@
 class Solution {
     public int largestInteger(int num) {
 
-    PriorityQueue<Integer> oddHeap =new PriorityQueue<>((x, y) -> Integer.compare(y, x));
-    PriorityQueue<Integer> evenHeap =new PriorityQueue<>((x, y) -> Integer.compare(y, x));    
+    PriorityQueue<Integer> oddHeap =new PriorityQueue<>(Collections.reverseOrder());
+    PriorityQueue<Integer> evenHeap =new PriorityQueue<>(Collections.reverseOrder());    
 
     int temp = num;
     while( num > 0 ) { 
@@ -14,8 +14,6 @@ class Solution {
         num /=10;
     }
 
-    // System.out.println(evenHeap);
-    // System.out.println(oddHeap);
 
     int pow=1;
     String str= String.valueOf(temp);
@@ -25,7 +23,7 @@ class Solution {
 
         char ch = str.charAt(i);
         if(  Integer.parseInt(String.valueOf(ch)) %2 ==0 ) {
-            // System.out.println(Integer.parseInt(String.valueOf(ch)));
+      
             ans += String.valueOf(evenHeap.poll());
         }else { 
             ans += String.valueOf(oddHeap.poll());
