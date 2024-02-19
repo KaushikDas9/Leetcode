3ea78ee9a174601e702035ecf449007bff3b01db
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2024-02-19 19:03:45 +0530

    Java Solution
            Submission Statistics:
            Question #: 378
            Language: Java
            Runtime: 23 ms
            Runtime percentile: 16.38
            Memory: 47.784 MB
            Memory percentile: 89.42

---
+++ 378. Kth Smallest Element in a Sorted Matrix/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+
+        for( int i=0;i<matrix.length;i++ ) { 
+
+            for( int j=0;j<matrix.length;j++ ) { 
+
+                pq.add( matrix[i][j] );
+            }
+        }
+
+        while( k-->1) { 
+
+            pq.poll();
+        }
+
+        return pq.poll();
+
+    }
+}
