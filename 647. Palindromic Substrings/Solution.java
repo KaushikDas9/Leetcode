32d412806bba1caf7352ff85526a9896e964f004
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2024-02-29 21:43:26 +0530

    Java Solution
            Submission Statistics:
            Question #: 647
            Language: Java
            Runtime: 32 ms
            Runtime percentile: 34.66
            Memory: 57.352 MB
            Memory percentile: 5.59

--- 647. Palindromic Substrings/Solution.java
+++ 647. Palindromic Substrings/Solution.java
@@ -1,20 +1,23 @@
 class Solution {
 
 
-    public static boolean find( String s, int i,int j ) { 
+    public static int find( String s, int i,int j,Integer dp[][] ) { 
 
-        if( i>= j ) return true;
-        if( s.charAt(i) == s.charAt(j) ) return find( s,i+1,j-1 );
+        if( i>= j ) return 1;
+        if( dp[i][j] != null ) return dp[i][j];
+        if( s.charAt(i) == s.charAt(j) ) return dp[i][j]=find( s,i+1,j-1,dp );
 
-        return false ;
+        return dp[i][j]=0 ;
     } 
     public int countSubstrings(String s) {
         
         int count =0;
+        int n =s.length();
+        Integer dp[][] = new Integer[n+1][n+1];
         for( int i=0;i<s.length();i++ ) { 
 
             for( int j=i;j<s.length();j++ ) {
-                 if( find( s,i,j ) ) count++ ;
+                 if( find( s,i,j,dp ) == 1 ) count++ ;
             }
         } 
         return count;
