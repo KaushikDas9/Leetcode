a2987e013790b9f71c813cc3ce5ba6e0034e6645
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2024-03-01 13:23:14 +0530

    Java Solution
            Submission Statistics:
            Question #: 3055
            Language: Java
            Runtime: 4 ms
            Runtime percentile: 44.58
            Memory: 45.056 MB
            Memory percentile: 5.42

--- 3055. Maximum Odd Binary Number/Solution.java
+++ 3055. Maximum Odd Binary Number/Solution.java
@@ -1,20 +1,21 @@
 class Solution {
     public String maximumOddBinaryNumber(String s) {
 
-        int arr[] = new int[2];
+        int zero =0;
+        int one = 0;
 
         for( int i=0;i<s.length();i++ )  { 
             if( s.charAt(i) == '1' ) { 
-                arr[1] = arr[1] + 1 ;
+                one++;
             }else { 
-                arr[0] = arr[0] + 1 ;
+                zero++;
             }
         }
 
         String ans = "";
 
-      for(int i=0;i<arr[1]-1;i++ ) ans += '1';
-      for(int i=0;i<arr[0];i++ ) ans += '0';
+      for(int i=0;i<one-1;i++ ) ans += '1';
+      for(int i=0;i<zero;i++ ) ans += '0';
         ans+='1';
         return ans;
         
