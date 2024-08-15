7be27183bd8c40a1311c3d693410b8679363a8ef
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2024-08-15 18:55:35 +0530

    Time: 613 ms (74.66%) | Memory: 21.3 MB (15.58%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -1,27 +1,39 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
+        five = 0
+        ten =0
+        twen =0
+
         my_list= list()
 
         for i in range(0,len(bills)):
 
             if bills[i] == 5:
-                my_list.append(5)
+                five +=1
+                # my_list.append(5)
                
             elif bills[i] == 10:
-                if 5 in my_list:
-                    my_list.append(10)
-                    my_list.remove(5)
+                if five>=1:
+                    ten += 1
+                    five -= 1
+                    # my_list.append(10)
+                    # my_list.remove(5)
                 else:
                     return False
 
             else: 
-                if my_list.count(10)>=1 and my_list.count(5)>=1:
-                    my_list.append(20)
-                    my_list.remove(10)
-                    my_list.remove(5)
-                elif my_list.count(5)>=3:
-                    my_list.append(20)
-                    for i in range(3): my_list.remove(5)
+                if ten >= 1 and five>=1:
+                    twen +=1
+                    ten -=1
+                    five -= 1
+                    # my_list.append(20)
+                    # my_list.remove(10)
+                    # my_list.remove(5)
+                elif five>=3:
+                    twen += 1
+                    five -= 3
+                    # my_list.append(20)
+                    # for i in range(3): my_list.remove(5)
                 else:
                     return False  
 
