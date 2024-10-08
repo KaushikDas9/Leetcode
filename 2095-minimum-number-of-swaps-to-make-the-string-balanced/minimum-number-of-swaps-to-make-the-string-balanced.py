e122aea269f3fe04d8e644f7d1e8cc704a5ba19c
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2024-10-08 12:58:52 +0530

    Time: 431 ms (52.29%) | Memory: 63.2 MB (7.34%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -1,25 +1,31 @@
 class Solution(object):
     def minSwaps(self, s):
-
+        """
+        :type s: str
+        :rtype: int
+        """
+        # Convert the string to a list of characters
+        s = list(s)
+        
         n = len(s)
         o = 0
         c = 0
         ans = 0 
-        j=n-1
+        j = n - 1
+        
         for i in range(n):
             if s[i] == "[" : 
-                o +=1
+                o += 1
             else:
                 c += 1 
                 if c > o :
                     while s[j] != "[":
-                        j -= 1 
+                        j -= 1
+                    temp = s[i]
+                    s[i] = s[j]
+                    s[j] = temp
                     c -= 1
                     o += 1
                     ans += 1
-
-
-        return ans
-
-            
         
+        return ans
