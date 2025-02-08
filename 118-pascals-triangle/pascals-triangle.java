d4253b07b925a8db19856fe24ba9b0d01e479234
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2025-02-08 16:13:09 +0530

    Time: 1 ms (84.14%) | Memory: 42.1 MB (38.71%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,30 +1,25 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        
-        List<List<Integer>> ans = new ArrayList<>();
-        
-        if( numRows == 0 ) return ans;
-        
-         List<Integer> firstRow = new ArrayList<>();
-         firstRow.add(1);
-         ans.add( firstRow );
-       
 
-
-        for ( int i=1;i<numRows;i++ ) { 
-
-            List<Integer> prev = ans.get(i-1);
-            List<Integer> curr = new ArrayList<>();
-            curr.add(1);
-
-            for ( int j=1;j<i;j++ ) {
-                curr.add( prev.get(j)+prev.get(j-1) );
-            }
-
-            curr.add(1);
-            ans.add(curr);
+        //Recursive Solution
+        if( numRows==0 ) return new ArrayList<>();
+        if(numRows==1 ) {
+            List<List<Integer>> r = new ArrayList<>();
+            r.add(Arrays.asList(1));
+            return r;
         }
 
-        return ans;
+
+        List<List<Integer>> prev = generate(numRows-1);
+        List<Integer> curr = new ArrayList<>();
+        
+        for( int i=0;i<numRows;i++ ) curr.add(1);
+        
+        for ( int i=1;i<numRows-1;i++ ) { 
+            curr.set( i,prev.get(numRows-2).get(i-1) + prev.get(numRows-2).get(i) );
+        }
+        
+        prev.add(curr);
+        return prev;
     }
 }
