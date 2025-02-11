e44bd10a23d38b885af67ef547c7b09f41dcbb2e
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2025-02-12 03:43:53 +0530

    Time: 4 ms (8.60%) | Memory: 43.8 MB (5.06%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,77 @@
+class Solution {
+    
+    public int orangesRotting(int[][] grid) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+        int ans =0;
+        Queue<ArrayList<Integer>> q = new LinkedList<>();
+        int[][] vis = new int[n][m];
+
+        for( int i=0;i<n;i++ ) {
+            for( int j=0;j<m;j++ ) {
+                if( grid[i][j]==2  ) {
+                    ArrayList<Integer> l = new ArrayList<>(); 
+                    l.add(i); l.add(j); l.add(0);
+                    q.add(l);
+                }
+            }
+        }
+
+        while( !q.isEmpty() ) {
+
+            ArrayList<Integer> temp = q.poll();
+            int row = temp.get(0);
+            int col = temp.get(1);
+            int time = temp.get(2);
+            ans = time;
+            // left
+            if( (col-1)>=0 && grid[row][col-1]==1 && vis[row][col-1]==0 ) {
+                // q add , vis add ,time increase
+                vis[row][col-1]=2;
+                // grid[row][col-1]=2;
+                ArrayList<Integer> l = new ArrayList<>();
+                l.add(row); l.add(col-1); l.add(time+1);
+                q.add(l);
+            }
+            // right 
+            if( (col+1)<=m-1 && grid[row][col+1]==1 && vis[row][col+1]==0  ) {
+                // q add , vis add ,time increase
+                vis[row][col+1]=2;
+                // grid[row][col+1]=2;
+                ArrayList<Integer> l = new ArrayList<>();
+                l.add(row); l.add(col+1); l.add(time+1);
+                q.add(l);
+            }
+            // up 
+            if( (row-1)>=0 && grid[row-1][col]==1 && vis[row-1][col]==0 ) {
+                // q add , vis add ,time increase
+                vis[row-1][col]=2;
+                // grid[row-1][col]=2;
+                ArrayList<Integer> l = new ArrayList<>();
+                l.add(row-1); l.add(col); l.add(time+1);
+                q.add(l);
+            }
+            // down 
+            if( (row+1)<=n-1 && grid[row+1][col]==1 && vis[row+1][col]==0 ) {
+                // q add , vis add ,time increase
+                vis[row+1][col]=2;
+                // grid[row+1][col]=2;
+                ArrayList<Integer> l = new ArrayList<>();
+                l.add(row+1); l.add(col); l.add(time+1);
+                q.add(l);
+            }
+
+        }
+
+
+        for( int i=0;i<n;i++ ) {
+            for( int j=0;j<m;j++ ) {
+                if( grid[i][j] == 1 && vis[i][j] != 2   ) {
+                    return -1;
+                }
+            }
+        }
+        return ans;
+    }
+}
