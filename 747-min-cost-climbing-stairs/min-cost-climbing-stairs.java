a732c5c78f5dc6c23cbea4558b51d399b7c6e0af
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2025-03-19 03:14:46 +0530

    Time: 1 ms (38.60%) | Memory: 43.8 MB (9.84%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,20 +1,34 @@
 class Solution {
 
 
-    public static int find( int[] arr,int i,Integer[] dp ) { 
+    public static int find( int[] arr,int n,Integer[] dp ) { 
 
-        if( i>=arr.length ) return 0;
-        if( dp[i]!= null ) return dp[i];
+        if( n<0 ) return 0;
+        if( dp[n]!= null ) return dp[n];
 
-        int a = arr[i]+find(arr,i+1,dp);
-        int b = arr[i]+find(arr,i+2,dp);
+        int a = arr[n]+find(arr,n-1,dp);
+        int b = arr[n]+find(arr,n-2,dp);
 
-        return dp[i]=Math.min(a,b);
+        return dp[n]=Math.min(a,b);
     }
     public int minCostClimbingStairs(int[] cost) {
         
-        Integer[] dp = new Integer[cost.length];
-        return Math.min(find(cost,0,dp),find(cost,1,dp));
+        int n=cost.length;
+        Integer[] dp = new Integer[n];
+        // return Math.min(find(cost,n-1,dp),find(cost,n-2,dp));
+
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+
+        for( int i=2;i<n;i++) { 
+
+            int a = cost[i]+dp[i-1];
+            int b = cost[i]+dp[i-2];
+
+            dp[i]=Math.min(a,b);
+        }
+
         
+        return Math.min(dp[n-1],dp[n-2]);
     }
 }
