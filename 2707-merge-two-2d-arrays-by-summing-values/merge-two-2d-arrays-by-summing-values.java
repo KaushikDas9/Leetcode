e7119d1bc97c549003f2a3807c1f99f10651a883
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2025-03-21 09:17:36 +0530

    Time: 2 ms (43.27%) | Memory: 45.5 MB (30.71%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -1,59 +1,47 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        
-        // int max_index =0; 
-        // HashMap<Integer,Integer> map1= new HashMap<>();
-        // HashMap<Integer,Integer> map2= new HashMap<>();
-
-        // for( int[] i:nums1 ) { map1.put(i[0],i[1]); max_index=Math.max(max_index,i[0]); }
-        // for( int[] i:nums2 ) { map2.put(i[0],i[1]); max_index=Math.max(max_index,i[0]); }
-        
-        // int[][] ans = new int[max_index][2];
-
-        // for( int i=1;i<=max_index;i++ ) { 
-        //     int s=0;
-        //     s = (map1.getOrDefault(i, 0) + map2.getOrDefault(i, 0));
-        //     ans[i-1][0] = i; ans[i-1][1] = s;
-        // }
-
-        // return ans;
-        
-        int i=0,j=0,in=0;
-        int max_index= Math.max( nums1[nums1.length-1][0],nums2[nums2.length-1][0] ) ; 
-        int[][] ans = new int[max_index][2];
-        while( i<nums1.length&&j<nums2.length ) { 
-            
-            if( nums1[i][0]==nums2[j][0] ) { 
-                ans[in][0] = nums1[i][0] ;
-                ans[in][1] = nums1[i][1] + nums2[j][1] ;
-                i++;j++;in++;
-            } else if ( nums1[i][0]<=nums2[j][0] ) { 
-                ans[in][0] = nums1[i][0] ; 
-                ans[in][1] = nums1[i][1];
-                i++;in++;
-            } else if ( nums1[i][0]>=nums2[j][0] ) {
-                ans[in][0] = nums2[j][0] ; 
-                ans[in][1] = nums2[j][1];
-                j++;in++;
-            } 
-
+        if (nums1 == null || nums2 == null) {
+            return new int[0][0];
         }
 
-        while ( i<nums1.length )  { 
-            ans[in][0] = nums1[i][0] ; 
-            ans[in][1] = nums1[i][1];
-            i++;in++;
+        int index1 = 0, index2 = 0, resultIndex = 0;
+        int maxIndex = Math.max(nums1[nums1.length - 1][0], nums2[nums2.length - 1][0]);
+        int[][] ans = new int[maxIndex + 1][2];
+
+        while (index1 < nums1.length && index2 < nums2.length) {
+            if (nums1[index1][0] == nums2[index2][0]) {
+                ans[resultIndex][0] = nums1[index1][0];
+                ans[resultIndex][1] = nums1[index1][1] + nums2[index2][1];
+                index1++;
+                index2++;
+            } else if (nums1[index1][0] <= nums2[index2][0]) {
+                ans[resultIndex][0] = nums1[index1][0];
+                ans[resultIndex][1] = nums1[index1][1];
+                index1++;
+            } else {
+                ans[resultIndex][0] = nums2[index2][0];
+                ans[resultIndex][1] = nums2[index2][1];
+                index2++;
+            }
+            resultIndex++;
         }
 
-        while ( j<nums2.length )  { 
-            ans[in][0] = nums2[j][0] ;
-            ans[in][1] = nums2[j][1];
-            j++;in++;
+        while (index1 < nums1.length) {
+            ans[resultIndex][0] = nums1[index1][0];
+            ans[resultIndex][1] = nums1[index1][1];
+            index1++;
+            resultIndex++;
         }
 
-        return Arrays.stream(ans)
-        .filter(row -> row[0] != 0 || row[1] != 0)
-        .toArray(int[][]::new);
-        
+        while (index2 < nums2.length) {
+            ans[resultIndex][0] = nums2[index2][0];
+            ans[resultIndex][1] = nums2[index2][1];
+            index2++;
+            resultIndex++;
+        }
+
+        int[][] result = new int[resultIndex][2];
+        System.arraycopy(ans, 0, result, 0, resultIndex);
+        return result;
     }
 }
