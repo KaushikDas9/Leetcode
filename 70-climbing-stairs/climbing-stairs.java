3051ef1e244dbf4bc6ada1f619cafc26386675e1
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2025-03-30 16:04:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (17.84%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,6 +1,5 @@
 class Solution {
 
-
     public static int find( int n,int[] dp ) { 
 
         if( n<0 ) return 0;
@@ -13,6 +12,15 @@ class Solution {
         
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return find(n,dp);
+        // return find(n,dp);
+
+        dp[0]=1;
+        dp[1]=1;
+
+        for( int i=2;i<=n;i++ ) { 
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+
     }
 }
