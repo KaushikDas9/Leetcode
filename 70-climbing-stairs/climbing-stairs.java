db5484ffe192de10868c9c4588178c9730a1664d
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2025-03-30 16:05:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (37.62%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -10,12 +10,12 @@ class Solution {
     }
     public int climbStairs(int n) {
         
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
+        // int[] dp = new int[n+1];
+        // Arrays.fill(dp,-1);
         // return find(n,dp);
 
-        dp[0]=1;
-        dp[1]=1;
+        // dp[0]=1;
+        // dp[1]=1;
         int prev = 1;
         int prev2 = 1;
         for( int i=2;i<=n;i++ ) { 
