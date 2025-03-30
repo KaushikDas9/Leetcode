7363ed454c1a34fcd3ce2d29c0fcb752b0a9c35f
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2025-03-30 16:05:36 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (27.14%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -16,11 +16,14 @@ class Solution {
 
         dp[0]=1;
         dp[1]=1;
-
+        int prev = 1;
+        int prev2 = 1;
         for( int i=2;i<=n;i++ ) { 
-            dp[i]=dp[i-1]+dp[i-2];
+            int curr = prev2+prev;
+            prev2 = prev;
+            prev =curr;
         }
-        return dp[n];
+        return prev;
 
     }
 }
