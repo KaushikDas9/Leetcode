1005e7099694b96765cc4dfe34f883d10493e2d6
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2025-04-05 09:47:24 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (95.73%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,16 +1,17 @@
 class Solution {
 
     public static int find( int n,int xor,int[] arr ) { 
+        
+        //base case
+        if( n ==arr.length ) return xor;
 
-        if( n==arr.length ) return xor;
-
-        int take = find( n+1,arr[n]^xor,arr );
+        int take = find( n+1,xor^arr[n],arr );
         int ntake = find( n+1,xor,arr );
 
         return take+ntake;
     }
-
     public int subsetXORSum(int[] nums) {
-        return find( 0,0,nums);
+        
+        return find( 0,0,nums );
     }
 }
