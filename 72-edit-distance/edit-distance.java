c67defc164093e10bf7b43e5d7581335959e216f
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2025-04-06 18:14:04 +0530

    Time: 5 ms (64.67%) | Memory: 45 MB (78.01%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -19,7 +19,23 @@ class Solution {
         int n = word1.length();
         int m = word2.length();
 
-        Integer[][] dp = new Integer[n][m];
-        return find(0,0,word1,word2,dp);
+        int[][] dp = new int[n+1][m+1];
+        // return find(0,0,word1,word2,dp);
+        for(int i = 0; i <= n; i++ ) dp[i][0] = i;
+        for(int j = 0; j <= m; j++ ) dp[0][j] = j;
+
+        for(int i = 1; i <= n; i++ ) { 
+            for(int j = 1; j <= m; j++ ) { 
+                if( word1.charAt(i-1) == word2.charAt(j-1)) { 
+                    dp[i][j] = 0 + dp[i-1][j-1];
+                }else {
+                        dp[i][j] = 1 + Math.min( dp[i-1][j], 
+                            Math.min( dp[i][j-1],
+                                        dp[i-1][j-1] ) );
+                    }
+            }
+        }
+
+        return dp[n][m];
     }
 }
