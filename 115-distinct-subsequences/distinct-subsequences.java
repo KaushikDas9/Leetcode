7c41b8de2a51e0ba389d16aba3fe08f348b1e9c1
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2025-04-06 11:32:09 +0530

    Time: 20 ms (44.34%) | Memory: 51.4 MB (42.29%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -20,7 +20,22 @@ class Solution {
         int n = s.length();
         int m = t.length();
 
-        Integer[][] dp = new Integer[n][m];
-        return find(0,0,s,t,dp);        
+        int[][] dp = new int[n + 1][m + 1];
+        // return find(0,0,s,t,dp);
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+        
+        for(int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) { 
+                int ans=0;
+                if(s.charAt(i - 1) == t.charAt(j - 1) ) {
+                    ans = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else { 
+                    ans = dp[i - 1][j];
+                }
+                dp[i][j] = ans;
+            }
+        }
+
+        return dp[n][m];
     }
 }
