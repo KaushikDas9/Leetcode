92877f9ee8c985b803d9dc9c13c5b0dfb4b9f31d
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2025-04-12 11:49:54 +0530

    Time: 436 ms (5.05%) | Memory: 194.8 MB (5.39%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -2,16 +2,16 @@ class Solution {
 
     public static int find( int i , int prev , int[] arr , Integer[][] dp ) { 
         
-        if ( i == arr.length ) return 0;
+        if ( i < 0 ) return 0;
         if( dp[i][prev+1] != null ) return dp[i][prev+1];
 
         int take = 0 , ntake = 0 ;
         
-        if( prev == -1 || arr[prev] < arr[i] ) { 
-            take= 1 + find( i + 1 , i , arr , dp );
+        if( prev == arr.length || arr[prev] > arr[i] ) { 
+            take= 1 + find( i - 1 , i , arr , dp );
         }
 
-        ntake = 0 + find( i + 1 , prev , arr , dp );
+        ntake = 0 + find( i - 1 , prev , arr , dp );
         
         return dp[i][prev+1] = Math.max( take , ntake );
 
@@ -21,7 +21,8 @@ class Solution {
 
         int n = nums.length;
 
-        Integer[][] dp = new Integer[n][n+1];
-        return find( 0,-1,nums,dp );
+        Integer[][] dp = new Integer[n][n+2];
+        return find( n-1,n,nums,dp );
+        
     }
 }
