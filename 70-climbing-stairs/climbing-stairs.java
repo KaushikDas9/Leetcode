b32ffbd40511ca92c170501d7f22e100b53aea69
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2025-05-08 11:28:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (50.81%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,29 +1,32 @@
 class Solution {
 
-    public static int find( int n,int[] dp ) { 
 
-        if( n<0 ) return 0;
+    public static int find( int n , Integer[] dp ) {
+        
         if( n == 0 ) return 1;
-        if( dp[n]!=-1 ) return dp[n];
-        return dp[n]=find(n-1,dp)+find(n-2,dp);
+        if( n < 0 ) return 0;
+
+        if( dp[n] != null ) return dp[n];
+
+        int l = 0;
+        int r = 0;
+
+        return dp[n] = find( n - 1  , dp ) + find( n - 2  , dp );
+
 
     }
     public int climbStairs(int n) {
-        
-        // int[] dp = new int[n+1];
-        // Arrays.fill(dp,-1);
-        // return find(n,dp);
 
-        // dp[0]=1;
-        // dp[1]=1;
-        int prev = 1;
-        int prev2 = 1;
-        for( int i=2;i<=n;i++ ) { 
-            int curr = prev2+prev;
-            prev2 = prev;
-            prev =curr;
+        int[] dp = new int[n+1]; 
+        // return find( n , dp );
+
+        dp[0] = 1 ; dp[1] = 1;
+
+        for ( int i = 2 ; i <= n ; i++ ) {
+            dp[i] = dp[ i - 1 ] + dp[ i - 2 ] ;
         }
-        return prev;
 
+        return dp[n];
     }
 }
+
