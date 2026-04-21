d7111de611de336e54a2e6df039974941ee9ccee
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2026-04-21 15:49:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (25.44%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -3,58 +3,37 @@ class Solution {
         
         int n = nums.length;
         if( n==1 ) return nums[0];
-        // return Math.max(find(0 , - 2 , nums , n - 1) , find(1 , - 2 , nums , n ));
         
         if( n == 2 ) return Math.max(nums[0] , nums[1]);
         if( n == 3 ) return  Math.max((Math.max(nums[1] , nums[2])) , (Math.max(nums[0] , nums[1])));
 
-
-        // step 1 :
-        int[] dp1 = new int[n];
-
-        // base case
-        dp1[0] = nums[0];
-        dp1[1] = Math.max(nums[0] , nums[1]);
+        int prev1 = nums[0];
+        int curr1 = Math.max(nums[0] , nums[1]);
 
         for( int i = 2; i < n - 1; i++ ) {
 
-            int t = nums[i] + dp1[i - 2];
-            int nt = dp1[i - 1];
+            int t = nums[i] + prev1;
+            int nt = curr1;
 
-            dp1[i] = Math.max(t , nt);
+            prev1 = curr1;
+            curr1 = Math.max(t , nt);
         }
 
-        int[] dp2 = new int[n];
-
         // base case
-        dp2[1] = nums[1];
-        dp2[2] = Math.max(nums[1] , nums[2]);
+        int prev2 = nums[1];
+        int curr2 = Math.max(nums[1] , nums[2]);
 
         for( int i = 3; i < n; i++ ) {
 
-            int t = nums[i] + dp2[i - 2];
-            int nt = dp2[i - 1];
+            int t = nums[i] + prev2;
+            int nt = curr2;
 
-            dp2[i] = Math.max(t , nt);
+            prev2 = curr2;
+            curr2 = Math.max(t , nt);
+
         }
 
-        return Math.max(dp1[n-2] , dp2[n-1]);
+        return Math.max(curr1 , curr2);
     }
 
-    // public static int find( int n , int[] arr , Integer[] dp) {
-
-    //     // base case
-    //     if( n >= arr.length ) return 0;
-
-    //     // check hit 
-    //     if(dp[n] != null) return dp[n]; 
-
-
-    //     int take = arr[n] + find(n + 2 , arr ,dp);
-
-    //     int ntake = 0 + find(n + 1 , arr ,dp);
-
-    //     return dp[n] = Math.max(take , ntake);
-
-    // }
 }
